1f04d14b76d7cea393b41abcef7b701c8dcf5827
aminesaibi <58223634+aminesaibi@users.noreply.github.com>
2019-11-28 12:22:18 +0100

    Add files via upload

---
+++ Board/main.cpp
@@ -0,0 +1,31 @@
+#include "Board.hpp"
+
+
+
+
+int main(int argc, char *argv[])
+{
+    Board b;
+    b.wallgen();
+    b.wallgen();
+    b.wallgen();
+    b.wallgen();
+    b.wallgen();
+    b.wallgen();
+    b.wallgen();
+    b.wallgen();
+    b.wallgen();
+    b.wallgen();
+    b.wallgen();
+    b.wallgen();
+    b.wallgen();
+    b.wallgen();
+    b.wallgen();
+    b.wallgen();
+    b.wallgen();
+    b.wallgen();
+    b.wallgen();
+    b.prt();
+    
+    return 0;
+}
