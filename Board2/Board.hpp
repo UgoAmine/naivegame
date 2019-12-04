86e42fe3083e64a635ddaf311d72fac5593a44fc
aminesaibi <58223634+aminesaibi@users.noreply.github.com>
2019-12-04 10:31:49 +0100

    Add files via upload

---
+++ Board2/Board.hpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <random>     // mt19937 (Mersene Twister) and uniform_int_distribution
+#include <algorithm>  // generate (used for random data)
+#include <vector>     // vector
+#include <iterator>   // begin, end, and ostream_iterator
+#include <functional> // bind (used for random data generator)
+#include <ncurses.h>
+
+std::vector<int> random_data(int n , int a, int b); //random data generator function(number of data, lower bound, upperbound )
+
+class Board
+{
+private:
+    int size;
+    
+public:
+Board();
+Board(int );
+
+~Board();
+};
---
+++ Board2/main.cpp
@@ -0,0 +1,11 @@
+#include "Board.hpp"
+
+
+
+
+int main(int argc, char *argv[])
+{
+    Board b;
+    
+    return 0;
+}
