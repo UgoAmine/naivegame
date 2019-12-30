a522acbfa02c53ccb2f2e73e7fb89afbc2efb464
UgoMassariol <58076147+UgoMassariol@users.noreply.github.com>
2019-12-30 18:45:25 +0100

    Update Monstre.cpp

--- jeu/Monstre.cpp
+++ jeu/Monstre.cpp
@@ -1,25 +1,88 @@
-#ifndef MONSTRE_H
-#define MONSTRE_H
+#include "Monstre.hpp"
+#include <iostream>
 
-#include "Board.hpp"
 
-class Monstre
+Monstre::Monstre(WINDOW* win)
 {
-public:
-    Monstre(WINDOW*);
-    virtual ~Monstre();
+    //CREATES A VECTOR OF RANDOM STARTING POS FOR MONSTER
+    setpos({random_data(1,2,48)[0],random_data(1,2,98)[0]});
 
-    inline std::vector<int> getpos() const {return posM;}
-    void setpos(const std::vector<int> );
+    //PRINTS MONSTER ON ITS INITIAL RANDOM POSITION
+    mvwaddch(win, posM[0],posM[1], 'S');
+}
 
-    void move(WINDOW*);
-    void moveNorth(WINDOW*);
-    void moveEast(WINDOW*);
-    void moveSouth(WINDOW*);
-    void moveWest(WINDOW*);
+Monstre::~Monstre(){}
 
-private:
-    std::vector<int> posM;
-};
+void Monstre::setpos(const std::vector<int> p) {posM = p;}
 
-#endif // MONSTRE_H
+void Monstre::move(WINDOW* win)
+{
+  switch(random_data(1,1,4)[0])
+  {
+    case 1:
+        this->moveNorth(win);
+      break;
+    case 2:
+        this->moveEast(win);
+      break;
+    case 3:
+        this->moveSouth(win);
+      break;
+    case 4:
+        this->moveWest(win);
+      break;
+  }
+  wrefresh(win);
+}
+
+void Monstre::moveNorth(WINDOW* window)
+{
+  char goal = mvwinch(window, posM[0]-1, posM[1]);
+  if (goal == 'J')
+    mvwaddch(window, 0, 0, ' ');
+  else if ((goal != 'X') && (goal != '+') && (goal != '-'))
+  {
+    mvwaddch(window,posM[0],posM[1], ' ');
+    posM[0]--;
+    mvwaddch(window,posM[0],posM[1], 'S');
+  }
+}
+
+void Monstre::moveEast(WINDOW* window)
+{
+  char goal = mvwinch(window, posM[0], posM[1]+1);
+  if (goal == 'J')
+    mvwaddch(window, 0, 0, ' ');
+  else if ((goal != 'X') && (goal != '+') && (goal != '-'))
+  {
+    mvwaddch(window,posM[0],posM[1], ' ');
+    posM[1]++;
+    mvwaddch(window,posM[0],posM[1], 'S');
+  }
+}
+
+void Monstre::moveSouth(WINDOW* window)
+{
+  char goal = mvwinch(window, posM[0]+1, posM[1]);
+  if (goal == 'J')
+    mvwaddch(window, 0, 0, ' ');
+  else if ((goal != 'X') && (goal != '+') && (goal != '-'))
+  {
+    mvwaddch(window,posM[0],posM[1], ' ');
+    posM[0]++;
+    mvwaddch(window,posM[0],posM[1], 'S');
+  }
+}
+
+void Monstre::moveWest(WINDOW* window)
+{
+  char goal = mvwinch(window, posM[0], posM[1]-1);
+  if (goal == 'J')
+    mvwaddch(window, 0, 0, ' ');
+  else if ((goal != 'X') && (goal != '+') && (goal != '-'))
+  {
+    mvwaddch(window,posM[0],posM[1], ' ');
+    posM[1]--;
+    mvwaddch(window,posM[0],posM[1], 'S');
+  }
+}
