a7cd84d5fa63d187384f764b36538fb8592d36db
UgoMassariol <58076147+UgoMassariol@users.noreply.github.com>
2019-12-30 18:43:49 +0100

    Update Joueur.cpp

--- jeu/Joueur.cpp
+++ jeu/Joueur.cpp
@@ -1,10 +1,9 @@
 #include "Joueur.hpp"
 #include <iostream>
 
-
-Player::Player(WINDOW* win)
+Player::Player(WINDOW* win) : numberOfTeleport(0)
 {
-    //  CREATES A VECTOR OF RANDOM STARTING POS FOR PLAYER
+    //  CREATES RANDOM STARTING POS FOR PLAYER (AS A VECTOR)
     setpos({random_data(1,2,48)[0],random_data(1,2,98)[0]});
 
     //  PRINTS PLAYER ON ITS INITIAL RANDOM POSITION
@@ -12,111 +11,27 @@ Player::Player(WINDOW* win)
     wrefresh(win);
 }
 
-void Player::moveNorth(WINDOW* window)
-{
-  mvwaddch(window,posJ[0],posJ[1], ' ');
-  posJ[0]--;
-  mvwaddch(window,posJ[0],posJ[1], 'J');
-}
-
-void Player::moveEast(WINDOW* window)
-{
-  mvwaddch(window,posJ[0],posJ[1], ' ');
-  posJ[1]++;
-  mvwaddch(window,posJ[0],posJ[1], 'J');
-}
-
-void Player::moveSouth(WINDOW* window)
-{
-  mvwaddch(window,posJ[0],posJ[1], ' ');
-  posJ[0]++;
-  mvwaddch(window,posJ[0],posJ[1], 'J');
-}
-
-void Player::moveWest(WINDOW* window)
-{
-  mvwaddch(window,posJ[0],posJ[1], ' ');
-  posJ[1]--;
-  mvwaddch(window,posJ[0],posJ[1], 'J');
-}
-
-void Player::doorOpening()
-{
-  //A IMPLEMENTER
-}
-
-void Player::telepIncrem()
-{
-  //A IMPLEMENTER
-}
+Player::~Player(){}
 
 void Player::move(char c, WINDOW* win)
 {
   switch(c)
   {
     case 'z': //  NORTH
-      if (mvwinch(win, posJ[0]-1, posJ[1]) == '$')
-      {
-        this->doorOpening();
-        this->moveNorth(win);
-      }
-      else if (mvwinch(win, posJ[0]-1, posJ[1]) == '*')
-      {
-        this->telepIncrem();
-        this->moveNorth(win);
-      }
-      else if (mvwinch(win, posJ[0]-1, posJ[1]) == ' ')
-      {
-        this->moveNorth(win);
-      }
+      this->moveNorth(win);
+      this->action(win, mvwinch(win, posJ[0]-1, posJ[1]));
       break;
     case 'd': //  EAST
-      if (mvwinch(win, posJ[0], posJ[1]+1) == '$')
-      {
-        this->doorOpening();
-        this->moveEast(win);
-      }
-      else if (mvwinch(win, posJ[0], posJ[1]+1) == '*')
-      {
-        this->telepIncrem();
-        this->moveEast(win);
-      }
-      else if (mvwinch(win, posJ[0], posJ[1]+1) == ' ')
-      {
-        this->moveEast(win);
-      }
+      this->moveEast(win);
+      this->action(win, mvwinch(win, posJ[0], posJ[1]+1));
       break;
     case 'q': //  WEST
-      if (mvwinch(win, posJ[0], posJ[1]-1) == '$')
-      {
-        this->doorOpening();
-        this->moveWest(win);
-      }
-      else if (mvwinch(win, posJ[0], posJ[1]-1) == '*')
-      {
-        this->telepIncrem();
-        this->moveWest(win);
-      }
-      else if (mvwinch(win, posJ[0], posJ[1]-1) == ' ')
-      {
-        this->moveWest(win);
-      }
+      this->moveWest(win);
+      this->action(win, mvwinch(win, posJ[0], posJ[1]-1));
       break;
     case 's': //  SOUTH
-      if (mvwinch(win, posJ[0]+1, posJ[1]) == '$')
-      {
-        this->doorOpening();
-        this->moveSouth(win);
-      }
-      else if (mvwinch(win, posJ[0]+1, posJ[1]) == '*')
-      {
-        this->telepIncrem();
-        this->moveSouth(win);
-      }
-      else if (mvwinch(win, posJ[0]+1, posJ[1]) == ' ')
-      {
-        this->moveSouth(win);
-      }
+      this->moveSouth(win);
+      this->action(win, mvwinch(win, posJ[0]+1, posJ[1]));
       break;
   }
 
@@ -125,7 +40,83 @@ void Player::move(char c, WINDOW* win)
 
 void Player::setpos(const std::vector<int> p) {posJ = p;}
 void Player::setposy(const int y) {posJ[0] = y;}
-void Player::setposx(const int x) {posJ[0] = x;}
+void Player::setposx(const int x) {posJ[1] = x;}
 
 
-Player::~Player(){}
+void Player::moveNorth(WINDOW* window)
+{
+  char goal = mvwinch(window, posJ[0]-1, posJ[1]);
+  if (goal == 'S')
+    mvwaddch(window, 0, 0, ' ');
+  else if (goal != 'X' && goal != '-')
+  {
+    mvwaddch(window,posJ[0],posJ[1], ' ');
+    posJ[0]--;
+    mvwaddch(window,posJ[0],posJ[1], 'J');
+  }
+}
+
+void Player::moveEast(WINDOW* window)
+{
+  char goal = mvwinch(window, posJ[0], posJ[1]+1);
+  if (goal == 'S')
+    mvwaddch(window, 0, 0, ' ');
+  else if (goal != 'X' && goal != '-')
+  {
+    mvwaddch(window,posJ[0],posJ[1], ' ');
+    posJ[1]++;
+    mvwaddch(window,posJ[0],posJ[1], 'J');
+  }
+}
+
+void Player::moveSouth(WINDOW* window)
+{
+  char goal = mvwinch(window, posJ[0]+1, posJ[1]);
+  if (goal == 'S')
+    mvwaddch(window, 0, 0, ' ');
+  else if (goal != 'X' && goal != '-')
+  {
+    mvwaddch(window,posJ[0],posJ[1], ' ');
+    posJ[0]++;
+    mvwaddch(window,posJ[0],posJ[1], 'J');
+  }
+}
+
+void Player::moveWest(WINDOW* window)
+{
+  char goal = mvwinch(window, posJ[0], posJ[1]-1);
+  if (goal == 'S')
+    mvwaddch(window, 0, 0, ' ');
+  else if (goal != 'X' && goal != '-')
+  {
+    mvwaddch(window,posJ[0],posJ[1], ' ');
+    posJ[1]--;
+    mvwaddch(window,posJ[0],posJ[1], 'J');
+  }
+}
+
+void Player::action(WINDOW* window, char goal)
+{
+  if (goal == '*')
+    this->numberOfTeleport++;
+  else if (goal == '$')
+  {/*
+    int choixFrontiere{random_data(1,1,6)[0]};
+    int i = 0;
+    switch (choixFrontiere)
+    {
+      case 1:
+        break;
+      case 2:
+        break;
+      case 3:
+        break;
+      case 4:
+        break;
+      case 5:
+        break;
+      case 6:
+        break;
+    }*/
+  }
+}
