b48f1f8255d8d31ed389132e1d2dade3160b51f5
UgoMassariol <58076147+UgoMassariol@users.noreply.github.com>
2019-12-30 18:44:41 +0100

    Update Monstre.cpp

--- jeu/Monstre.cpp
+++ jeu/Monstre.cpp
@@ -1,58 +1,25 @@
-#include "Monstre.hpp"
-#include <iostream>
+#ifndef MONSTRE_H
+#define MONSTRE_H
 
+#include "Board.hpp"
 
-Monstre::Monstre(WINDOW* win)
+class Monstre
 {
-    //CREATES A VECTOR OF RANDOM STARTING POS FOR MONSTER
-    setpos({random_data(1,2,48)[0],random_data(1,2,98)[0]});
+public:
+    Monstre(WINDOW*);
+    virtual ~Monstre();
 
-    //PRINTS MONSTER ON ITS INITIAL RANDOM POSITION
-    mvwaddch(win, posM[0],posM[1], 's');
-}
+    inline std::vector<int> getpos() const {return posM;}
+    void setpos(const std::vector<int> );
 
-void Monstre::move(WINDOW* win)
-{
-    switch(random_data(1,1,4)[0])
-            {
-                case 1:
-                    if(mvwinch(win,posM[0]-1,posM[1])==' ')
-                    {
-                        mvwaddch(win,posM[0],posM[1], ' ');
-                        posM[0]--;
-                        mvwaddch(win,posM[0],posM[1], 'S');
-                    }
-                    break;
-                case 2:
-                    if(mvwinch(win,posM[0],posM[1]+1)!='X')
-                    {
-                        mvwaddch(win,posM[0],posM[1], ' ');
-                        posM[1]++;
-                        mvwaddch(win,posM[0],posM[1], 'S');
-                    }
-                    break;
-                case 3:
-                    if(mvwinch(win,posM[0]+1,posM[1])!='X')
-                    {
-                        mvwaddch(win,posM[0],posM[1], ' ');
-                        posM[0]++;
-                        mvwaddch(win,posM[0],posM[1], 'S');
-                    }
-                    break;
-                case 4:
-                    if(mvwinch(win,posM[0],posM[1]-1)!='X')
-                    {
-                        mvwaddch(win,posM[0],posM[1], ' ');
-                        posM[1]--;
-                        mvwaddch(win,posM[0],posM[1], 'S');
-                    }
-                    break;
-            }
-wrefresh(win);
-}
+    void move(WINDOW*);
+    void moveNorth(WINDOW*);
+    void moveEast(WINDOW*);
+    void moveSouth(WINDOW*);
+    void moveWest(WINDOW*);
 
-void Monstre::setpos(std::vector<int> p) {
-    posM=p;
-}
+private:
+    std::vector<int> posM;
+};
 
-Monstre::~Monstre(){}
+#endif // MONSTRE_H
